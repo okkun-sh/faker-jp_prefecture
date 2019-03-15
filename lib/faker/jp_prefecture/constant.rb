515c07c63b6354d4d1b348727975dfadf875378b
okkun_sh <okkun.sh@gmail.com>
2019-03-15 23:57:36 +0900

    added name_f

--- lib/faker/jp_prefecture/constant.rb
+++ lib/faker/jp_prefecture/constant.rb
@@ -2,236 +2,283 @@ PREFECTURES = [
   {
     code: 1,
     name: "北海道",
-    name_e: "hokkaido"
+    name_e: "Hokkaido",
+    name_f: "ほっかいどう",
   },
   {
     code: 2,
     name: "青森県",
-    name_e: "aomori"
+    name_e: "Aomoriken",
+    name_f: "あおもりけん",
   },
   {
     code: 3,
     name: "岩手県",
-    name_e: "iwate"
+    name_e: "Iwateken",
+    name_f: "いわてけん",
   },
   {
     code: 4,
     name: "宮城県",
-    name_e: "miyagi"
+    name_e: "Miyagiken",
+    name_f: "みやぎけん",
   },
   {
     code: 5,
     name: "秋田県",
-    name_e: "akita"
+    name_e: "Akitaken",
+    name_f: "あきたけん",
   },
   {
     code: 6,
     name: "山形県",
-    name_e: "yamagata"
+    name_e: "Yamagataken",
+    name_f: "やまがたけん",
   },
   {
     code: 7,
     name: "福島県",
-    name_e: "fukushima"
+    name_e: "Fukushimaken",
+    name_f: "ふくしまけん",
   },
   {
     code: 8,
     name: "茨城県",
-    name_e: "ibaraki"
+    name_e: "Ibarakiken",
+    name_f: "いばらきけん",
   },
   {
     code: 9,
     name: "栃木県",
-    name_e: "tochigi"
+    name_e: "Tochigiken",
+    name_f: "とちぎけん",
   },
   {
     code: 10,
     name: "群馬県",
-    name_e: "gunma"
+    name_e: "Gunmaken",
+    name_f: "ぐんまけん",
   },
   {
     code: 11,
     name: "埼玉県",
-    name_e: "saitama"
+    name_e: "Saitamaken",
+    name_f: "さいたまけん",
   },
   {
     code: 12,
     name: "千葉県",
-    name_e: "chiba"
+    name_e: "Chibaken",
+    name_f: "ちばけん",
   },
   {
     code: 13,
     name: "東京都",
-    name_e: "tokyo"
+    name_e: "Tokyoto",
+    name_f: "とうきょうと",
   },
   {
     code: 14,
     name: "神奈川県",
-    name_e: "kanagawa"
+    name_e: "Kanagawaken",
+    name_f: "かながわけん",
   },
   {
     code: 15,
     name: "新潟県",
-    name_e: "niigata"
+    name_e: "Niigataken",
+    name_h: "にいがたけん",
   },
   {
     code: 16,
     name: "富山県",
-    name_e: "toyama"
+    name_e: "Toyamaken",
+    name_f: "とやまけん",
   },
   {
     code: 17,
     name: "石川県",
-    name_e: "ishikawa"
+    name_e: "Ishikawaken",
+    name_f: "いしかわけん",
   },
   {
     code: 18,
     name: "福井県",
-    name_e: "fukui"
+    name_e: "Fukuiken",
+    name_f: "ふくいけん",
   },
   {
     code: 19,
     name: "山梨県",
-    name_e: "yamanashi"
+    name_e: "Yamanashiken",
+    name_f: "やまなしけん",
   },
   {
     code: 20,
     name: "長野県",
-    name_e: "nagano"
+    name_e: "Naganoken",
+    name_f: "ながのけん",
   },
   {
     code: 21,
     name: "岐阜県",
-    name_e: "gifu"
+    name_e: "Gifuken",
+    name_f: "ぎふけん",
   },
   {
     code: 22,
     name: "静岡県",
-    name_e: "shizuoka"
+    name_e: "Shizuokaken",
+    name_f: "しずおかけん",
   },
   {
     code: 23,
     name: "愛知県",
-    name_e: "aichi"
+    name_e: "Aichiken",
+    name_f: "あいちけん",
   },
   {
     code: 24,
     name: "三重県",
-    name_e: "mie"
+    name_e: "Mieken",
+    name_f: "みえけん",
   },
   {
     code: 25,
     name: "滋賀県",
-    name_e: "shiga"
+    name_e: "Shigaken",
+    name_f: "しがけん",
   },
   {
     code: 26,
     name: "京都府",
-    name_e: "kyoto"
+    name_e: "Kyotofu",
+    name_f: "きょうとふ",
   },
   {
     code: 27,
     name: "大阪府",
-    name_e: "osaka"
+    name_e: "Osakafu",
+    name_f: "おおさかふ",
   },
   {
     code: 28,
     name: "兵庫県",
-    name_e: "hyogo"
+    name_e: "Hyogoken",
+    name_f: "ひょうごけん",
   },
   {
     code: 29,
     name: "奈良県",
-    name_e: "nara"
+    name_e: "Naraken",
+    name_f: "ならけん",
   },
   {
     code: 30,
     name: "和歌山県",
-    name_e: "wakayama"
+    name_e: "Wakayamaken",
+    name_f: "わかやまけん",
   },
   {
     code: 31,
     name: "鳥取県",
-    name_e: "tottori"
+    name_e: "Tottoriken",
+    name_f: "とっとりけん",
   },
   {
     code: 32,
     name: "島根県",
-    name_e: "shimane"
+    name_e: "Shimaneken",
+    name_f: "しまねけん",
   },
   {
     code: 33,
     name: "岡山県",
-    name_e: "okayama"
+    name_e: "Okayamaken",
+    name_f: "おかやまけん",
   },
   {
     code: 34,
     name: "広島県",
-    name_e: "hiroshima"
+    name_e: "Hiroshimaken",
+    name_f: "ひろしまけん",
   },
   {
     code: 35,
     name: "山口県",
-    name_e: "yamaguchi"
+    name_e: "Yamaguchiken",
+    name_f: "やまぐちけん",
   },
   {
     code: 36,
     name: "徳島県",
-    name_e: "tokushima"
+    name_e: "Tokushimaken",
+    name_f: "とくしまけん",
   },
   {
     code: 37,
     name: "香川県",
-    name_e: "kagawa"
+    name_e: "Kagawaken",
+    name_f: "かがわけん",
   },
   {
     code: 38,
     name: "愛媛県",
-    name_e: "ehime"
+    name_e: "Ehimeken",
+    name_f: "えひめけん",
   },
   {
     code: 39,
     name: "高知県",
-    name_e: "kochi"
+    name_e: "Kochiken",
+    name_f: "こうちけん",
   },
   {
     code: 40,
     name: "福岡県",
-    name_e: "fukuoka"
+    name_e: "Fukuokaken",
+    name_f: "ふくおかけん",
   },
   {
     code: 41,
     name: "佐賀県",
-    name_e: "saga"
+    name_e: "Sagaken",
+    name_f: "さがけん",
   },
   {
     code: 42,
     name: "長崎県",
-    name_e: "nagasaki"
+    name_e: "Nagasakiken",
+    name_f: "ながさきけん",
   },
   {
     code: 43,
     name: "熊本県",
-    name_e: "kumamoto"
+    name_e: "Kumamoto",
+    name_f: "くまもとけん",
   },
   {
     code: 44,
     name: "大分県",
-    name_e: "oita"
+    name_e: "Oitaken",
+    name_f: "おおいたけん",
   },
   {
     code: 45,
     name: "宮崎県",
-    name_e: "miyazaki"
+    name_e: "Miyazakiken",
+    name_f: "みやざきけん",
   },
   {
     code: 46,
     name: "鹿児島県",
-    name_e: "kagoshima"
+    name_e: "Kagoshimaken",
+    name_f: "かごしまけん",
   },
   {
     code: 47,
     name: "沖縄県",
-    name_e: "okinawa"
+    name_e: "Okinawaken",
+    name_f: "おきなわけん",
   }
 ]
